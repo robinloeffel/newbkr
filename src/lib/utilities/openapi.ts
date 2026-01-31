6c0277f572da13d708bb9d856054f2d29a714729
Robin Löffel <6851646+robinloeffel@users.noreply.github.com>
2026-01-31 22:19:09 +0100

    feat: .env files

--- src/lib/utilities/openapi.ts
+++ src/lib/utilities/openapi.ts
@@ -1,8 +1,7 @@
+import { PUBLIC_OPENAPI_BASE_URL } from "$env/static/public";
+import type { paths } from "$types/ibkr";
 import createClient from "openapi-fetch";
-import type { paths } from "../types/ibkr";
 
 export const client = createClient<paths>({
-  baseUrl: import.meta.env.DEV
-    ? "https://qa.interactivebrokers.com/"
-    : "https://api.ibkr.com/"
+  baseUrl: PUBLIC_OPENAPI_BASE_URL
 });
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,12 @@
 <script lang="ts">
+  import { client } from "$utilities/openapi";
   import { onMount } from "svelte";
 
-  onMount(() => {
+  onMount(async () => {
+    const { data, error } = await client.POST("/tickle");
+
     // eslint-disable-next-line no-console
-    console.log("yes");
+    console.log({ data, error });
   });
 </script>
 
